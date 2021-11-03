4a034e9b819d6f94fa0cdfad2192c229ceaa707f
Chirag Goel <76050897+Chirag-creator17@users.noreply.github.com>
2021-11-03 15:20:31 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,129 +1,102 @@
 #include<stdio.h>
-#include<graphics.h>
 #include<conio.h>
+#include<graphics.h>
 #include<dos.h>
-void paths(){
-	// paths
-	rectangle(0,170,220,0);
-		setfillstyle(SOLID_FILL,GREEN);
-		floodfill(100,100,WHITE);
-	rectangle(420,0,getmaxx(),170);
-		setfillstyle(SOLID_FILL,GREEN);
-		floodfill(430,100,WHITE);
-	rectangle(0,320,220,getmaxy());
-		setfillstyle(SOLID_FILL,GREEN);
-		floodfill(210,330,WHITE);
-	rectangle(420,320,getmaxx(),getmaxy());
-		setfillstyle(SOLID_FILL,GREEN);
-		floodfill(450,350,WHITE);
-	// signal 1
-	rectangle(205,335,185,410);
-		setfillstyle(SOLID_FILL,YELLOW);
-		floodfill(200,350,WHITE);
-	circle(195,350,8);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(195,350,WHITE);
-	circle(195,372,8);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(195,372,WHITE);
-	circle(195,394,8);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(195,394,WHITE);
-	// signal 2
-	rectangle(435,335,510,355);
-		setfillstyle(SOLID_FILL,YELLOW);
-		floodfill(500,340,WHITE);
-	circle(450,345,8);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(450,345,WHITE);
-	circle(472,345,8);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(472,345,WHITE);
-	circle(492,345,8);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(492,345,WHITE);
-}
-void car1(int i){
-	rectangle(530 - i,300,630 - i,270);
-	rectangle(548 - i,270,570 - i,240);
-	circle(550 - i,300,10);
-	circle(610 - i,300,10);
-}
-void car2(int i){
-	rectangle(400,100 + i,370,20 + i);
-	rectangle(370,88 + i,350,70 + i);
-	circle(400 + i,80,10);
-	circle(400 + i,40,10);
-}
-void signal1(int i){
-	if(i==1){
-		setfillstyle(SOLID_FILL,RED);
-		floodfill(195,350,WHITE);
-		delay(500);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(195,350,WHITE);
-		setfillstyle(SOLID_FILL,YELLOW);
-		floodfill(195,372,WHITE);
-		delay(500);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(195,372,WHITE);
-		setfillstyle(SOLID_FILL,GREEN);
-		floodfill(195,394,WHITE);
+void car(int x);
+int main()
+{
+int gd=DETECT,gn;
+int x;
+char ch;
+initgraph(&gd,&gn,"C:\\TURBOC3\\BGI");
+x=180;
+ch='r';
+while(ch!='e')
+{
+	cleardevice();
+	rectangle(50,150,100,50);
+	rectangle(90,62,60,140);
+	circle(75,75,10);
+	circle(75,100,10);
+	circle(75,125,10);
+	car(x);
+	switch(ch)
+	{
+	case 'r':setfillstyle(SOLID_FILL,RED);
+		 rectangle(90,62,60,140);
+		 circle(75,75,10);
+		 floodfill(75,75,WHITE);
+		 circle(75,100,10);
+		 circle(75,125,10);
+		 break;
+	case 'g':setfillstyle(SOLID_FILL,GREEN);
+		 rectangle(90,62,60,140);
+		 circle(75,75,10);
+		 circle(75,125,10);
+		 floodfill(75,125,WHITE);
+		 circle(75,100,10);
+		 while(!kbhit())
+		 {
+			x-=2;
+			if(x<-100)
+				x=180;
+			car(x);
+			circle(75,75,10);
+			rectangle(90,62,60,140);
+			circle(75,125,10);
+			floodfill(75,125,WHITE);
+			circle(75,100,10);
+			delay(50);
+		 }
+		 break;
+	case 'y':setfillstyle(SOLID_FILL,YELLOW);
+		 rectangle(90,62,60,140);
+		 circle(75,75,10);
+		 circle(75,100,10);
+		 circle(75,125,10);
+		 floodfill(75,100,WHITE);
+		 delay(1000);
+		 setfillstyle(SOLID_FILL,RED);
+		 floodfill(75,75,WHITE);
+		 setfillstyle(SOLID_FILL,BLACK);
+		 floodfill(75,100,WHITE);
+		 break;
+	default: printf("Thank you\nPress e to Exit");
+		 break;
 	}
-	else{
-		setfillstyle(SOLID_FILL,GREEN);
-		floodfill(195,394,WHITE);
-		delay(500);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(195,394,WHITE);
-		setfillstyle(SOLID_FILL,YELLOW);
-		floodfill(195,372,WHITE);
-		delay(500);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(195,372,WHITE);
-		setfillstyle(SOLID_FILL,RED);
-		floodfill(195,350,WHITE);
-	}
+	delay(50);
+	ch=getch();
 }
-void signal2(int i){
-	if(i==1){
-		setfillstyle(SOLID_FILL,RED);
-		floodfill(450,345,WHITE);
-		delay(500);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(450,345,WHITE);
-		setfillstyle(SOLID_FILL,YELLOW);
-		floodfill(472,345,WHITE);
-		delay(500);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(472,345,WHITE);
-		setfillstyle(SOLID_FILL,GREEN);
-		floodfill(494,345,WHITE);
-	}
-	else{
-		setfillstyle(SOLID_FILL,GREEN);
-		floodfill(494,345,WHITE);
-		delay(500);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(494,345,WHITE);
-		setfillstyle(SOLID_FILL,YELLOW);
-		floodfill(472,345,WHITE);
-		delay(500);
-		setfillstyle(SOLID_FILL,BLACK);
-		floodfill(472,345,WHITE);
-		setfillstyle(SOLID_FILL,RED);
-		floodfill(450,345,WHITE);
-	}
+getch();
+return 0;
 }
-void main(){
-	int gd=DETECT,gm;
-	clrscr();
-	initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
-	paths();
-	car1(0);
-	car2(0);
-	signal1(1);
-	signal2(1);
-	getch();
-	closegraph();
+void car(int i)
+{
+	cleardevice();
+	line(0,350,getmaxx(),350);
+	line(100+i,260,100+i,320);
+	line(450+i,240,450+i,320);
+	line(100+i,260,180+i,240);
+	line(180+i,240,400+i,240);
+	line(450+i,240,400+i,240);
+	line(180+i,240,210+i,170);
+	line(400+i,240,400+i,170);
+	line(310+i,220,310+i,210);
+	line(330+i,220,330+i,210);
+	line(210+i,170,400+i,170);
+	line(235+i,170,235+i,240);
+	line(320+i,170,320+i,240);
+	circle(180+i,320,30);
+	line(130+i,320,150+i,280);
+	line(230+i,320,210+i,280);
+	line(150+i,280,210+i,280);
+	circle(380+i,320,30);
+	line(330+i,320,350+i,280);
+	line(430+i,320,410+i,280);
+	line(350+i,280,410+i,280);
+	line(100+i,320,130+i,320);
+	line(230+i,320,330+i,320);
+	line(450+i,320,430+i,320);
+	circle(380+i,320,5);
+	circle(180+i,320,5);
 }
